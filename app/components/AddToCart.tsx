4540068402cac495c9d013a4bacece12abc82348
ksk-nexbe <email@example.com>
2024-02-21 12:14:48 +0630

    first init

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AddToCart = () => {
+  return (
+    <div>
+        <button className='btn btn-primary' onClick={()=> console.log('hello world')}>Add to Cart</button>
+
+    </div>
+  )
+}
+
+export default AddToCart
